31dd923818a6a3e307c02826796b214d9d6b7b39
hehesheng <hehesheng.hk@outlook.com>
2024-04-02 16:33:14 +0800

    feat: continue play preview video && fix: optimize js code

--- js/rtc.player.page.js
+++ js/rtc.player.page.js
@@ -1,3 +1,4 @@
+'use strict';
 $(function () {
     var sdk = null; // Global handler to do cleanup when replaying.
     // graph value
@@ -11,8 +12,18 @@ $(function () {
     let networkDelaySeries;
 
     let maxRenderTime = -1;
-    let maxNetworkDelay = -1;
     const windowSize = 30;
+    function setCookie(name, value) {
+        document.cookie = `${name}=${value}`;
+    }
+    function getCookie(name, if_null) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) { 
+            return parts.pop().split(';').shift();
+        }
+        return if_null;
+    }
     function init_status_graph() {
         fpsSeries = new TimelineDataSeries();
         fpsGraph = new TimelineGraphView('fpsGraph', 'fpsCanvas');
@@ -29,15 +40,23 @@ $(function () {
         networkDelayGraph.setScale(200);
         networkDelayGraph.updateEndDate();
     }
+    let max_delay_value_input = document.getElementById("max_delay_value");
     function set_max_delay() {
+        let max_delay_ms = max_delay_value_input.value;
+        setCookie("delayMs", max_delay_ms);
         if (sdk) {
-            let max_delay_ms = document.getElementById("max_delay_value").value;
-            let [aRec, vRec] = sdk.pc.getReceivers();
             // console.info(max_delay_ms);
-            aRec.playoutDelayHint = max_delay_ms / 1000;
-            vRec.playoutDelayHint = max_delay_ms / 1000;
+            sdk.pc.getReceivers().forEach(function(rec, index, arr) {
+                rec.playoutDelayHint = max_delay_ms / 1000;
+            });
         }
     }
+    max_delay_value_input.value = Number(getCookie("delayMs", 0));
+    max_delay_value_input.onkeydown = function (e) {
+        if (e.key == "Enter") {
+            set_max_delay();
+        }
+    };
     init_status_graph();
     var startPlay = function () {
         $('#rtc_media_player').show();
@@ -45,6 +64,7 @@ $(function () {
         // Close PC when user replay.
         if (sdk) {
             sdk.close();
+            sdk = null;
         }
         sdk = new SrsRtcPlayerAsync();
 
@@ -60,6 +80,7 @@ $(function () {
             $('#simulator-drop').attr('href', session.simulator + '?drop=1&username=' + session.sessionid);
         }).catch(function (reason) {
             sdk.close();
+            sdk = null;
             $('#rtc_media_player').hide();
             console.error(reason);
         });
@@ -88,14 +109,26 @@ $(function () {
         get_record_list(query, $("#record_stream_name").val());
     }
 
-    document.querySelector("#refresh_record_file_button").onclick = function () {
+    function query_record_stream_by_name() {
         get_record_list(query, $("#record_stream_name").val());
+    }
+    document.getElementById("record_stream_name").onkeydown = function (e) {
+        if (e.key == "Enter") {
+            query_record_stream_by_name();
+        }
+    };
+    document.getElementById("refresh_record_file_button").onclick = query_record_stream_by_name;
+
+    let playback_check_box = document.getElementById("playback_check_box");
+    playback_check_box.checked = getCookie("playback", "false") === "true";
+    playback_check_box.onchange = function (e) {
+        setCookie("playback", playback_check_box.checked);
     };
 
     function get_record_list(query, stream_name) {
         let web_protocal = window.location.protocol;
-        base_url = web_protocal + "//" + query.hostname + (web_protocal == "http:" ? ":11985" : "");
-        query_url = base_url + "/stream/query_record/" + stream_name;
+        let base_url = web_protocal + "//" + query.hostname + (web_protocal == "http:" ? ":11985" : "");
+        let query_url = base_url + "/stream/query_record/" + stream_name;
         const record_file_list_query = new XMLHttpRequest();
         record_file_list_query.open("GET", query_url);
         record_file_list_query.send();
@@ -106,7 +139,7 @@ $(function () {
             if (record_file_list_query.readyState === 4 && record_file_list_query.status === 200) {
                 record_request_status.style.display = "none";
                 let record_file_list_infos = JSON.parse(record_file_list_query.responseText);
-                let record_file_list_obj = document.querySelector("#record_file_list");
+                let record_file_list_obj = document.getElementById("record_file_list");
                 record_file_list_obj.innerHTML = '';
                 if (record_file_list_infos.stream_name != stream_name) {
                     return;
@@ -118,16 +151,34 @@ $(function () {
                     file_item.style = "text-align: left; border: 1px solid black;";
                     // preview button
                     let preview_record_file_button = document.createElement("button");
-                    preview_record_file_button.className = "btn btn-primary";
+                    preview_record_file_button.className = "btn btn-primary previewBtn";
+                    preview_record_file_button.style.width = "10%";
                     preview_record_file_button.innerHTML = "preview";
-                    function preview_button_click (record_info) {
+                    preview_record_file_button.id = file_info.file_name + "_preview_btn";
+                    function play_next_call(e, pre_info) {
+                        let enable_countine_play = playback_check_box.checked;
+                        if (!enable_countine_play) { return; }
+                        let next_ele = document.getElementById(pre_info.file_name + "_preview_btn").parentElement.previousElementSibling;
+                        // console.info(e, " info:", pre_info, " next:", next_ele);
+                        if (next_ele == null) { return; }
+                        let btn_list = next_ele.getElementsByClassName("btn");
+                        if (btn_list.length == 0) { return; }
+                        let b = btn_list[0];
+                        b.click();
+                    }
+                    function play_record_file(record_info, btn) {
                         if (record_info.file_name.split(".").slice(-1) != "mp4") {
-                            alert("仅支持mp4录制文件！");
+                            alert("仅支持mp4文件预览！");
                             return;
                         }
                         if (sdk) {
                             sdk.close();
                             sdk = null;
+                        }
+                        // reset btn
+                        let preview_btn_list = document.getElementsByClassName("previewBtn");
+                        for (let i = 0; i < preview_btn_list.length; i++) {
+                            preview_btn_list[i].innerHTML = "preview";
                         }
                         // preview_url = file_url;
                         let media_player = document.getElementById("rtc_media_player");
@@ -135,9 +186,13 @@ $(function () {
                         media_player.style.display = "";
                         $('#rtc_media_player').prop('srcObject', null);
                         media_player.src = base_url + "/stream/record/p/" + record_info.file_name;
+                        media_player.onended = function (e) {
+                            play_next_call(e, record_info);
+                        };
+                        btn.innerHTML = "on playing";
                     }
                     preview_record_file_button.addEventListener("click", function () {
-                        preview_button_click(file_info);
+                        play_record_file(file_info, preview_record_file_button);
                     });
                     file_item.appendChild(preview_record_file_button);
                     file_item.appendChild(document.createTextNode('\n'));
@@ -173,8 +228,8 @@ $(function () {
 
     function add_refresh_stream_list_button(url) {
         // add refresh button
-        let streams_list = document.querySelector("#streams_grid");
-        let user_name_selector = document.querySelector("#user_name");
+        let streams_list = document.getElementById("streams_grid");
+        let user_name_selector = document.getElementById("user_name");
         let refresh_button = document.createElement("button");
         streams_list.style.gap = "1vh";
         refresh_button.className = "btn btn-primary";
@@ -202,8 +257,8 @@ $(function () {
             if (Http.readyState === 4 && Http.status === 200) {
                 // add_refresh_stream_list_button(url);
                 // console.log(Http.responseText);
-                let streams_list = document.querySelector("#streams_grid");
-                let user_name_selector = document.querySelector("#user_name");
+                let streams_list = document.getElementById("streams_grid");
+                let user_name_selector = document.getElementById("user_name");
                 let server_streams_status = JSON.parse(Http.responseText);
                 console.log(server_streams_status);
                 server_streams_status.streams.forEach((stream, index, arr) => {
@@ -238,8 +293,8 @@ $(function () {
         window.addEventListener("load", function () { startPlay(); });
     }
     // Part 1:
-    var vid = document.querySelector("#rtc_media_player");
-    var last_media_time, last_frame_num, fps;
+    var vid = document.getElementById("rtc_media_player");
+    var last_media_time, last_frame_num;
     var last_metadata = null;
     let frame_info_rounder = [];
     class FrameInfo {
@@ -252,7 +307,10 @@ $(function () {
     function frameBeginCallback(nowMs, metaData) {
         let media_time_diff = Math.abs(metaData.mediaTime - last_media_time);
         let frame_num_diff = Math.abs(metaData.presentedFrames - last_frame_num);
-        let diff = media_time_diff / frame_num_diff;
+        let diff = 0;
+        if (frame_num_diff !== 0) {
+            diff = media_time_diff / frame_num_diff;
+        }
         if (diff) {
             frame_info_rounder.push(new FrameInfo(frame_num_diff, Date.now()));
         }
@@ -262,14 +320,15 @@ $(function () {
         // console.info(nowMs);
         // console.info(metaData);
         // For graph purposes, take the maximum over a window.
-        maxRenderTime = Math.max(metaData.expectedDisplayTime - metaData.receiveTime, maxRenderTime);
+        if (metaData.receiveTime != null) {
+            maxRenderTime = Math.max(metaData.expectedDisplayTime - metaData.receiveTime, maxRenderTime);
+            if (metaData.presentedFrames % windowSize === 0) {
+                timeSeries.addPoint(Date.now(), maxRenderTime);
+                timeGraph.setDataSeries([timeSeries]);
+                timeGraph.updateEndDate();
 
-        if (metaData.presentedFrames % windowSize === 0) {
-            timeSeries.addPoint(Date.now(), maxRenderTime);
-            timeGraph.setDataSeries([timeSeries]);
-            timeGraph.updateEndDate();
-    
-            maxRenderTime = -1;
+                maxRenderTime = -1;
+            }
         }
         vid.requestVideoFrameCallback(frameBeginCallback);
     }
@@ -280,7 +339,7 @@ $(function () {
     });
     // Part 4:
     function update_frame_info(fps, width, height) {
-        document.querySelector("#frame_info").textContent = "Video Info: " + width + "x" + height + ", " + fps + "FPS";
+        document.getElementById("frame_info").textContent = "Video Info: " + width + "x" + height + ", " + fps + "FPS";
     }
     let audio_bytes_pre;
     let video_bytes_pre;
@@ -288,34 +347,25 @@ $(function () {
     let video_ts_pre;
     let total_rtt_pre;
     let responses_rev_pre;
-    let jitter_buffer_delay_pre;
-    let jitter_buffer_emitted_count_pre;
     function showRemoteStats(results) {
         // calculate video bitrate
         let audio_bps = "0";
         let video_bps = "0";
-        let jitter_delay = 0;
-        var rtt = 0;
+        let rtt = NaN;
         results.forEach(report => {
             const now = report.timestamp;
             if (report.type === 'inbound-rtp' && report.mediaType === 'video') {
                 const bytes = report.bytesReceived;
-                if (video_ts_pre) {
+                if (video_ts_pre && (now !== video_ts_pre)) {
                     video_bps = 8 * (bytes - video_bytes_pre) / (now - video_ts_pre);
                     video_bps = Math.floor(video_bps);
                 }
                 video_bytes_pre = bytes;
                 video_ts_pre = now;
-                if (jitter_buffer_emitted_count_pre) {
-                    jitter_delay = (report.jitterBufferDelay - jitter_buffer_delay_pre) /
-                        (report.jitterBufferEmittedCount - jitter_buffer_emitted_count_pre);
-                }
-                jitter_buffer_delay_pre = report.jitterBufferDelay;
-                jitter_buffer_emitted_count_pre = report.jitterBufferEmittedCount;
                 // console.info(report);
             } else if (report.type === 'inbound-rtp' && report.mediaType === 'audio') {
                 const bytes = report.bytesReceived;
-                if (audio_ts_pre) {
+                if (audio_ts_pre && (now !== audio_ts_pre)) {
                     audio_bps = 8 * (bytes - audio_bytes_pre) / (now - audio_ts_pre);
                     audio_bps = Math.floor(audio_bps);
                 }
@@ -323,7 +373,7 @@ $(function () {
                 audio_ts_pre = now;
                 // console.info(report);
             } else if (report.type === 'candidate-pair' && report.responsesReceived !== 0) {
-                if (total_rtt_pre) {
+                if (total_rtt_pre && (report.responsesReceived !== responses_rev_pre)) {
                     rtt = (report.totalRoundTripTime - total_rtt_pre) / (report.responsesReceived - responses_rev_pre);
                 }
                 total_rtt_pre = report.totalRoundTripTime;
@@ -334,16 +384,14 @@ $(function () {
         audio_bps += 'kbps';
         video_bps += 'kbps';
         // console.info(results);
-        document.querySelector("#bitrate_info").textContent = `video:${video_bps} audio:${audio_bps}`;
+        document.getElementById("bitrate_info").textContent = `video:${video_bps} audio:${audio_bps}`;
         let date_now = Date.now();
-        // jitter
-        // timeSeries.addPoint(date_now, jitter_delay * 1000);
-        // timeGraph.setDataSeries([timeSeries]);
-        // timeGraph.updateEndDate();
         // rtt
-        networkDelaySeries.addPoint(date_now, rtt * 1000);
-        networkDelayGraph.setDataSeries([networkDelaySeries]);
-        networkDelayGraph.updateEndDate();
+        if (rtt !== NaN) {
+            networkDelaySeries.addPoint(date_now, rtt * 1000);
+            networkDelayGraph.setDataSeries([networkDelaySeries]);
+            networkDelayGraph.updateEndDate();
+        }
     }
     setInterval(function () {
         if (sdk) {
@@ -352,6 +400,9 @@ $(function () {
         }
     }, 500);
     setInterval(function () {
+        if (vid.played.length === 0) {
+            return;
+        }
         let now_ms = Date.now();
         while (frame_info_rounder.length > 0 && now_ms - frame_info_rounder[0].time > 1000) {
             frame_info_rounder.shift();
